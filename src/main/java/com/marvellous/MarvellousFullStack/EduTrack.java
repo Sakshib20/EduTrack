d8ee6e1c6bff5acf3ad04219ed58486dac2b3a6b
Sakshib20 <154331302+Sakshib20@users.noreply.github.com>
2026-01-21 17:36:20 +0530

    EduTrack backend setup

---
+++ src/main/java/com/marvellous/MarvellousFullStack/EduTrack.java
@@ -0,0 +1,15 @@
+package com.marvellous.MarvellousFullStack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduTrack
+{
+
+	public static void main(String[] args)
+    {
+		SpringApplication.run(EduTrack.class, args);
+	}
+
+}
